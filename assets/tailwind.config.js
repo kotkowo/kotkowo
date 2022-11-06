31a1a84efc116d83a2596f7ba423d5054c615a65
Maksymilian Jodłowski <maksymilian.jodlowski@gmail.com>
2022-11-06 21:50:18 +0100

    :lipstick: Navbar

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -2,6 +2,7 @@
 // https://tailwindcss.com/docs/configuration
 
 const plugin = require("tailwindcss/plugin")
+const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
   content: [
@@ -12,15 +13,20 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        brand: "#FD4F00",
+        primary: '#116858',
+        'primary-light': '#F3F8F6',
+        'highlight': '#FEEC8F'
+      },
+      fontFamily: {
+        'sans': ['Inter', ...defaultTheme.fontFamily.sans]
       }
     },
   },
   plugins: [
     require("@tailwindcss/forms"),
-    plugin(({addVariant}) => addVariant("phx-no-feedback", [".phx-no-feedback&", ".phx-no-feedback &"])),
-    plugin(({addVariant}) => addVariant("phx-click-loading", [".phx-click-loading&", ".phx-click-loading &"])),
-    plugin(({addVariant}) => addVariant("phx-submit-loading", [".phx-submit-loading&", ".phx-submit-loading &"])),
-    plugin(({addVariant}) => addVariant("phx-change-loading", [".phx-change-loading&", ".phx-change-loading &"]))
+    plugin(({ addVariant }) => addVariant("phx-no-feedback", [".phx-no-feedback&", ".phx-no-feedback &"])),
+    plugin(({ addVariant }) => addVariant("phx-click-loading", [".phx-click-loading&", ".phx-click-loading &"])),
+    plugin(({ addVariant }) => addVariant("phx-submit-loading", [".phx-submit-loading&", ".phx-submit-loading &"])),
+    plugin(({ addVariant }) => addVariant("phx-change-loading", [".phx-change-loading&", ".phx-change-loading &"]))
   ]
 }
--- lib/kotkowo_web/components/layouts.ex
+++ lib/kotkowo_web/components/layouts.ex
@@ -1,5 +1,55 @@
 defmodule KotkowoWeb.Layouts do
   use KotkowoWeb, :html
 
+  @type nested_link :: {String.t(), String.t()}
+  @type css_classes :: [String.t()]
+  @type link :: {String.t(), String.t() | nil, [nested_link()], css_classes()}
+
+  @contact_email "kotkowo@email.com"
+  @links [
+    {"Aktualności", nil,
+     [
+       {"Z ostatniej chwili", "#"},
+       {"Znalazły dom", "#"},
+       {"Za tęczowym mostem", "#"}
+     ], []},
+    {"Adopcja", nil,
+     [
+       {"Szukają domu", "#"},
+       {"Zasady adopcji", "#"},
+       {"Adoptuj wirtualnie kota", "#"}
+     ], []},
+    {"Zaginione/znalezione", "#zaginione-znalezione", [], []},
+    {"Jak pomóc", nil,
+     [
+       {"Przekaż nam 1% podatku", "#"},
+       {"Wesprzyj nas finansowo", "#"},
+       {"Przekaż rzeczy dla kotów", "#"},
+       {"Zorganizuj zbiórkę", "#"},
+       {"Załóż dom tymczasowy", "#"},
+       {"Zapisz się na wolontariat", "#"}
+     ], ["p-2", "bg-highlight"]},
+    {"Porady", nil,
+     [
+       {"Czy to już czas na kota?", "#"},
+       {"Przygotuj dom na kota", "#"},
+       {"Jak opiekować się kotem?", "#"},
+       {"Co robić, gdy znajdę kota?", "#"},
+       {"Zachowanie kota", "#"}
+     ], []},
+    {"O nas", nil,
+     [
+       {"Historia fundacji", "#"},
+       {"Partnerzy", "#"},
+       {"Dokumenty", "#"}
+     ], []}
+  ]
+
+  @spec links :: [link()]
+  def links, do: @links
+
+  @spec contact_email :: String.t()
+  def contact_email, do: @contact_email
+
   embed_templates "layouts/*"
 end
