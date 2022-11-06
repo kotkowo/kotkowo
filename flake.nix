f39db10bdc591936d069afc1e2da0bbadfe532de
Maksymilian Jodłowski <maksymilian.jodlowski@gmail.com>
2022-11-06 14:34:55 +0100

    :snowflake: Add flake.nix

---
+++ flake.nix
@@ -0,0 +1,63 @@
+{
+  description = "Development environment";
+
+  inputs = {
+      nixpkgs = { url = "github:NixOS/nixpkgs/nixpkgs-unstable"; };
+      flake-utils = { url = "github:numtide/flake-utils"; };
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        inherit (nixpkgs.lib) optional;
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+
+        nodeDependencies = (pkgs.callPackage ./assets/default.nix {}).nodeDependencies;
+      in
+      {
+          devShell = pkgs.mkShell
+          {
+            buildInputs =
+              (with pkgs; [
+                elixir_1_14
+                elixir_ls
+                hex
+                rebar3
+                glibcLocales
+                nodejs
+                inotify-tools
+                # needed for emacs to unzip elixir-ls
+                unzip
+                # used to manage js using nix
+                node2nix
+              ]);
+
+              shellHook = ''
+                echo "Setting up mix"
+                # this allows mix to work on the local directory
+                mkdir -p .nix-mix
+                mkdir -p .nix-hex
+                export MIX_HOME=$PWD/.nix-mix
+                export HEX_HOME=$PWD/.nix-hex
+                export PATH=$MIX_HOME/bin:$PATH
+                export PATH=$HEX_HOME/bin:$PATH
+                export LANG=en_US.UTF-8
+                export PATH=$PATH:$(pwd)/_build/pip_packages/bin
+                export ERL_AFLAGS="-kernel shell_history enabled"
+
+                if [ ! -f .nix-mix/rebar3 ]; then
+                  echo "Setting up rebar3"
+                  mix local.rebar rebar3 ${pkgs.rebar3}/bin/rebar3
+                fi
+
+                if [ ! -d ./assets/node_modules ]; then
+                  echo "Setting up node"
+                  ln -s ${nodeDependencies}/lib/node_modules ./assets/node_modules
+                fi
+            '';
+          };
+      }
+    );
+}
