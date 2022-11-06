2660913a252bea7af87b97387d54c799ad4fd701
Maksymilian Jodłowski <maksymilian.jodlowski@gmail.com>
2022-11-06 14:42:04 +0100

    🐛 Fix wrongly generated Swoosh API client

--- config/prod.exs
+++ config/prod.exs
@@ -12,7 +12,7 @@ import Config
 config :kotkowo, KotkowoWeb.Endpoint, cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Configures Swoosh API Client
-config :swoosh, :api_client, kotkowo.Finch
+config :swoosh, :api_client, Swoosh.ApiClient.Finch
 
 # Do not print debug messages in production
 config :logger, level: :info
