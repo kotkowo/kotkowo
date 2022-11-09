cabeb0867ec0d2f2ff5c2718f83b6dec06f85e67
Maksymilian Jodłowski <maksymilian.jodlowski@gmail.com>
2022-11-09 21:33:42 +0100

    :wrench: Setup excoveralls

--- mix.exs
+++ mix.exs
@@ -10,7 +10,14 @@ defmodule Kotkowo.MixProject do
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
       deps: deps(),
-      preferred_cli_env: [check: :test]
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        check: :test,
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
+      ]
     ]
   end
 
@@ -54,7 +61,8 @@ defmodule Kotkowo.MixProject do
       {:plug_cowboy, "~> 2.5"},
       {:gradient, github: "esl/gradient", only: [:dev, :test], runtime: false},
       {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
-      {:git_hooks, "~> 0.7.0", only: [:dev], runtime: false}
+      {:git_hooks, "~> 0.7.0", only: [:dev], runtime: false},
+      {:excoveralls, "~> 0.10", only: :test}
     ]
   end
 
