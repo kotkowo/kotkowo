b4dcd1c1c719e0702d52b787249e21f471c04a79
Maksymilian Jodłowski <maksymilian.jodlowski@gmail.com>
2022-11-09 21:06:36 +0100

    :pin: Pin phoenix version to 1.7.0-rc.0

--- mix.exs
+++ mix.exs
@@ -32,7 +32,7 @@ defmodule Kotkowo.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, github: "phoenixframework/phoenix", override: true},
+      {:phoenix, "~> 1.7.0-rc.0", override: true},
       {:phoenix_ecto, "~> 4.4"},
       {:ecto_sql, "~> 3.6"},
       {:postgrex, ">= 0.0.0"},
