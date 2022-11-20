67e99bfb41e3f0a8460caabb81c42bb2bf3e2680
Maksymilian Jodłowski <maksymilian.jodlowski@gmail.com>
2022-11-20 21:00:16 +0100

    :black_nib: Adjust cat cards for mobile

--- lib/kotkowo_web/components/cards.ex
+++ lib/kotkowo_web/components/cards.ex
@@ -35,40 +35,50 @@ defmodule KotkowoWeb.Components.Cards do
 
   def cat_card(assigns) do
     ~H"""
-    <div class="w-82 shrink-0 snap-center xl:snap-none">
+    <div class="w-60 lg:w-82 shrink-0 snap-center lg:snap-none">
       <div class="relative">
-        <a href={@share_href} class="absolute right-3 top-3 bg-white p-2.5 rounded-full opacity-60">
-          <.icon_share />
+        <a
+          href={@share_href}
+          class="absolute right-3 top-3 bg-white w-6 lg:w-10 h-6 lg:h-10 rounded-full lg:opacity-60 flex"
+        >
+          <.icon_share class="w-3 lg:w-5 h-3 lg:h-5 m-auto" />
         </a>
         <img src={@src} alt={@name} class="border border-1 rounded-t-2xl w-full object-cover h-48" />
       </div>
 
-      <div class="bg-white rounded-3xl w-auto px-5 py-3 flex flex-col gap-y-3 pb-6 relative -mt-5 border border-1">
+      <div class="bg-white rounded-3xl w-auto px-3 lg:px-5 py-2 lg:py-3 flex flex-col gap-y-3 pb-3 lg:pb-6 relative -mt-5 border border-1">
         <div class="flex justify-between">
-          <h3 class="font-manrope font-bold text-2xl"><%= @name %></h3>
+          <h3 class="lg:font-manrope font-semibold lg:font-bold text-lg lg:text-2xl"><%= @name %></h3>
           <%= if @sex == :male do %>
-            <.icon_male class="h-6 my-auto" />
+            <.icon_male class="w-5 lg:w-5.5 h-5 lg:h-5.5 my-auto" />
           <% else %>
-            <.icon_female class="h-6 my-auto" />
+            <.icon_female class="w-5 lg:w-5.5 h-5 lg:h-5.5 my-auto" />
           <% end %>
         </div>
 
         <div>
-          <.icon_paw class="inline mr-2" />
-          <%= Seniority.to_string(@seniority) %>
+          <.icon_paw class="inline mr-1 lg:mr-3 w-4 lg:w-5 h-4 lg:h-5" />
+          <span class="text-sm lg:text-base"><%= Seniority.to_string(@seniority) %></span>
         </div>
         <div>
-          <.icon_sthetoscope class="inline mr-2" />
-          <%= HealthStatus.to_string(@health_status) %>
+          <.icon_sthetoscope class="inline mr-1 lg:mr-3 w-4 lg:w-5 h-4 lg:h-5" />
+          <span class="text-sm lg:text-base"><%= HealthStatus.to_string(@health_status) %></span>
         </div>
         <div>
-          <.icon_scissors class="inline mr-2" />
-          <%= (@castrated && gettext("Po kastracji")) || gettext("Przed kastracją") %>
+          <.icon_scissors class="inline mr-1 lg:mr-3 w-4 lg:w-5 h-4 lg:h-5" />
+          <span class="text-sm lg:text-base">
+            <%= (@castrated && gettext("Po kastracji")) || gettext("Przed kastracją") %>
+          </span>
         </div>
 
-        <div :if={@tags != []} class="mt-3">
+        <div
+          :if={@tags != []}
+          class="lg:mt-3 overflow-y-auto flex spacing space-x-1 whitespace-nowrap"
+        >
           <%= for tag <- @tags do %>
-            <span class="border border-2 rounded-3xl p-3 text-gray-500"><%= tag %></span>
+            <span class="border border-2 rounded-3xl text-gray-500 text-sm lg:text-base py-2 lg:py-3 px-3 lg:px-4">
+              <%= tag %>
+            </span>
           <% end %>
         </div>
       </div>
