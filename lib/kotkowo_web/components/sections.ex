bcc686842f39ba32325ea8373f1e1b0b8daab5c5
Maksymilian Jodłowski <maksymilian.jodlowski@gmail.com>
2022-12-03 20:03:55 +0100

    :newspaper: Footer section

--- lib/kotkowo_web/components/sections.ex
+++ lib/kotkowo_web/components/sections.ex
@@ -5,6 +5,7 @@ defmodule KotkowoWeb.Components.Sections do
   use Phoenix.Component, global_prefixes: ~w(x-)
 
   import KotkowoWeb.Components.Icons
+  import KotkowoWeb.Components.Buttons
   import KotkowoWeb.Gettext
 
   @type nested_link :: {String.t(), String.t()}
@@ -135,7 +136,7 @@ defmodule KotkowoWeb.Components.Sections do
                     <.icon name="facebook" class="w-10 invert" />
                   </a>
                   <a href="#">
-                    <.icon name="messenger" class="w-10" />
+                    <.icon name="messenger_black" class="w-10 h-10" />
                   </a>
                 </div>
               </div>
@@ -167,6 +168,121 @@ defmodule KotkowoWeb.Components.Sections do
       <section class={["mx-auto max-w-3xl md:max-w-5xl lg:max-w-7xl", @class]} {@rest}>
         <%= render_slot(@inner_block) %>
       </section>
+    </div>
+    """
+  end
+
+  attr :class, :string, default: nil
+  attr :rest, :global
+
+  def footer(assigns) do
+    ~H"""
+    <div class="w-full bg-primary text-white py-16">
+      <footer
+        class={["mx-auto max-w-3xl md:max-w-5xl lg:max-w-7xl overflow-x-hidden", @class]}
+        {@rest}
+      >
+        <div class="grid auto-rows-auto xl:auto-cols-auto grid-flow-row xl:grid-flow-col xl:w-full xl:text-lg ml-6 gap-10">
+          <.footer_section>
+            <div class="flex space-x-6">
+              <img src="https://via.placeholder.com/80" class="w-20 h-20" alt="logo" />
+              <img src="https://via.placeholder.com/80" class="w-20 h-20" alt="logo" />
+            </div>
+
+            <h3 class="text-xl xl:text-2xl font-bold font-manrope">Fundacja kotkowo</h3>
+
+            <.footer_link href="#" class="flex">
+              <.icon name="envelope2" class="w-5 h-5 inline my-auto mr-2" />
+              <span class="my-auto">kotkowo@email.com</span>
+            </.footer_link>
+
+            <div class="flex">
+              <.icon name="city_scraper" class="w-5 h-5 inline mr-2" />
+              <div class="flex flex-col space-y-2">
+                <span>Adres biura</span>
+                <span>al. J. Piłsudskiego 26/31</span>
+                <span>15-446 Białystok</span>
+              </div>
+            </div>
+
+            <div class="flex space-x-6">
+              <.icon name="facebook" class="w-8 h-8 inline" />
+              <.icon name="messenger" class="w-8 h-8 inline" />
+            </div>
+          </.footer_section>
+
+          <.footer_section title="Mapa strony">
+            <.footer_link href="#">Aktualności</.footer_link>
+            <.footer_link href="#">Adopcja</.footer_link>
+            <.footer_link href="#">Jak pomóc?</.footer_link>
+            <.footer_link href="#">Porady</.footer_link>
+            <.footer_link href="#">O fundacji</.footer_link>
+            <.footer_link href="#">Znalezione koty</.footer_link>
+            <.footer_link href="#">Kontakt</.footer_link>
+          </.footer_section>
+
+          <.footer_section title="Dokumenty">
+            <.footer_link href="#" class="hover:text-white">Polityka prywatności</.footer_link>
+            <.footer_link href="#">RODO</.footer_link>
+            <.footer_link href="#">Polityka cookies</.footer_link>
+            <.footer_link href="#">Formularz adopcyjny</.footer_link>
+            <.footer_link href="#">Umowa adopcyjna</.footer_link>
+          </.footer_section>
+
+          <.footer_section title="Dane fundacji">
+            <dl class="grid grid-flow-row auto-rows-auto gap-4">
+              <.footer_definition term="KRS" description="0000345319" />
+              <.footer_definition term="NIP" description="9662018446" />
+              <.footer_definition term="REGON" description="20032337500000" />
+              <.footer_definition term="Bank" description="BOŚ SA O. w Bialymstoku" />
+              <.footer_definition term="Numer konta" description="87 1540 1216 2054 4458 2306 0001" />
+              <.footer_definition term="SWIFT/BIC" description="EBOSPLPWXXX" />
+              <.footer_definition term="IBAN" description="PL 87 1540 1216 2054 4458 2306 0001" />
+            </dl>
+
+            <.button href="#" type="secondary" class="!mt-8">Wesprzyj nas</.button>
+          </.footer_section>
+        </div>
+      </footer>
+    </div>
+    """
+  end
+
+  attr :href, :string, required: true
+  attr :class, :string, default: nil
+
+  attr :rest, :global
+
+  slot :inner_block, required: true
+
+  defp footer_link(assigns) do
+    ~H"""
+    <a href={@href} class={["hover:text-white", @class]} {@rest}><%= render_slot(@inner_block) %></a>
+    """
+  end
+
+  attr :title, :string, default: nil
+  slot :inner_block, required: true
+
+  defp footer_section(assigns) do
+    ~H"""
+    <section class="flex">
+      <div class="flex flex-col space-y-4 xl:mx-auto">
+        <h3 :if={@title != nil} class="text-xl xl:text-2xl font-bold font-manrope"><%= @title %></h3>
+        <%= render_slot(@inner_block) %>
+      </div>
+    </section>
+    """
+  end
+
+  attr :term, :string, required: true
+  attr :description, :string, required: true
+
+  defp footer_definition(assigns) do
+    ~H"""
+    <div class="xl:grid grid-flow-col auto-cols-max gap-3">
+      <dt class="font-bold"><%= @term %></dt>
+      <dd><%= @description %></dd>
     </div>
     """
   end
