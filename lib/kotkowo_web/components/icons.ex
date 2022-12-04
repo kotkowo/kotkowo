343075d6e4a3dfe592abebd47b95b91b1aae71ad
Maksymilian Jodłowski <maksymilian.jodlowski@gmail.com>
2022-12-04 23:38:33 +0100

    :alien: Add facebook_outline icon

--- lib/kotkowo_web/components/icons.ex
+++ lib/kotkowo_web/components/icons.ex
@@ -10,7 +10,7 @@ defmodule KotkowoWeb.Components.Icons do
     router: KotkowoWeb.Router,
     statics: ~w(images)
 
-  @all ~w(male female paw sthetoscope scissors share envelope facebook bars messenger chevron_up chevron_down city_scraper envelope2 messenger_black)
+  @all ~w(male female paw sthetoscope scissors share envelope facebook bars messenger chevron_up chevron_down city_scraper envelope2 messenger_black facebook_outline)
 
   attr :name, :string, values: @all
 
