a6c4867b01f956c54a04bbc0c461d53011710417
Maksymilian Jodłowski <maksymilian.jodlowski@gmail.com>
2023-02-21 23:21:03 +0100

    :sparkles: Add some icons

--- lib/kotkowo_web/components/icons.ex
+++ lib/kotkowo_web/components/icons.ex
@@ -10,7 +10,7 @@ defmodule KotkowoWeb.Components.Icons do
     router: KotkowoWeb.Router,
     statics: ~w(images)
 
-  @all ~w(male female paw sthetoscope scissors share envelope facebook bars messenger chevron_up chevron_down city_scraper envelope2 messenger_black facebook_outline)
+  @all ~w(male female paw sthetoscope scissors share envelope facebook bars messenger chevron_up chevron_down city_scraper envelope2 messenger_black facebook_outline eraser recycle medicine droplet bag fish round_container cookie)
 
   attr :name, :string, values: @all
 
