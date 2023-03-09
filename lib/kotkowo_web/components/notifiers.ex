af60e2be4388cb61ab33269a1d872d346b558b00
Maksymilian Jodłowski <maksymilian.jodlowski@gmail.com>
2023-03-09 05:30:56 +0100

    🛩 Fix position of notification

--- lib/kotkowo_web/components/notifiers.ex
+++ lib/kotkowo_web/components/notifiers.ex
@@ -25,14 +25,14 @@ defmodule KotkowoWeb.Components.Notifiers do
           x-show="percentage > 0"
           x-cloak
           class={[
-            "flex items-center absolute left-1/2 -translate-x-1/2 bottom-2",
+            "flex items-center fixed left-1/2 -translate-x-1/2 bottom-2",
             "bg-black text-white font-bold w-60 h-12 px-6 rounded-full select-none"
           ]}
           {%{"x-on:#{@event}.window" => "show"}}
         >
           <.icon :if={@icon != nil} name="clipboard_done" class="w-7 h-7" />
           <span class="mx-auto"><%= @message %></span>
-          <div class="w-48 h-1 bottom-0 left-6 absolute flex">
+          <div class="w-48 h-1 bottom-0 left-6 fixed flex">
             <span class="bg-primary-lighter h-full" x-bind:style="`width: ${percentage}%;`"></span>
           </div>
         </div>
