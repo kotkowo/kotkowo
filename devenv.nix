41c72f0f3523bd98dbf0924364f781df061badaa
Maksymilian Jodłowski <maksymilian.jodlowski@gmail.com>
2023-08-15 18:16:40 +0200

    📦 Update packages & environment

--- devenv.nix
+++ devenv.nix
@@ -1,12 +1,11 @@
-{ pkgs, ... }:
-
-
-let
-  erlang = pkgs.beam.packages.erlangR25;
-in
-{
-  env.LANG="en_US.UTF-8";
-  env.ERL_AFLAGS="-kernel shell_history enabled";
+{pkgs, ...}: let
+  erlang = pkgs.beam.packages.erlangR26;
+  elixir = erlang.elixir_1_15;
+  node = pkgs.nodejs_20;
+  elixir-ls = erlang.elixir-ls.override {elixir = elixir;};
+in {
+  env.LANG = "en_US.UTF-8";
+  env.ERL_AFLAGS = "-kernel shell_history enabled";
 
   enterShell = ''
     export MIX_HOME=$PWD/.nix-mix
@@ -16,21 +15,26 @@ in
     export PATH=$PATH:$(pwd)/_build/pip_packages/bin
   '';
 
-  packages = with pkgs; [
-    elixir_ls
-  ] ++ lib.optionals pkgs.stdenv.isLinux (with pkgs; [ inotify-tools ]);
+  packages = with pkgs;
+    [
+      elixir_ls
+    ]
+    ++ lib.optionals pkgs.stdenv.isLinux (with pkgs; [inotify-tools]);
 
   languages.elixir = {
     enable = true;
-    package = erlang.elixir_1_14;
+    package = elixir;
   };
 
-  languages.javascript.enable = true;
+  languages.javascript = {
+    enable = true;
+    package = node;
+  };
 
   services.postgres = {
     enable = true;
     initialDatabases = [
-      { name = "kotkowo_dev"; }
+      {name = "kotkowo_dev";}
     ];
     initialScript = ''
       CREATE ROLE postgres WITH LOGIN SUPERUSER PASSWORD 'postgres';
--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Kotkowo.MixProject do
     [
       app: :kotkowo,
       version: "0.1.0",
-      elixir: "~> 1.14",
+      elixir: "~> 1.15",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
@@ -40,18 +40,18 @@ defmodule Kotkowo.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.7.0-rc.3", override: true},
+      {:phoenix, "~> 1.7.6"},
       {:phoenix_ecto, "~> 4.4"},
       {:ecto_sql, "~> 3.6"},
       {:postgrex, ">= 0.0.0"},
       {:phoenix_html, "~> 3.0"},
-      {:phoenix_live_reload, "~> 1.2", only: :dev},
-      {:phoenix_live_view, "~> 0.18.3"},
+      {:phoenix_live_reload, "~> 1.4", only: :dev},
+      {:phoenix_live_view, "~> 0.19"},
       {:heroicons, "~> 0.5"},
       {:floki, ">= 0.30.0", only: :test},
-      {:phoenix_live_dashboard, "~> 0.7.2"},
+      {:phoenix_live_dashboard, "~> 0.8"},
       {:esbuild, "~> 0.5", runtime: Mix.env() == :dev},
-      {:tailwind, "~> 0.1.8", runtime: Mix.env() == :dev},
+      {:tailwind, "~> 0.2", runtime: Mix.env() == :dev},
       {:swoosh, "~> 1.3"},
       {:finch, "~> 0.13"},
       {:telemetry_metrics, "~> 0.6"},
