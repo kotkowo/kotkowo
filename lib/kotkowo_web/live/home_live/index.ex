158e570ac704128aed208dc5edd6bc796c410185
Maksymilian Jodłowski <maksymilian.jodlowski@gmail.com>
2023-09-20 01:01:51 +0200

    :sparkles: Move home to LV

---
+++ lib/kotkowo_web/live/home_live/index.ex
@@ -0,0 +1,7 @@
+defmodule KotkowoWeb.HomeLive.Index do
+  @moduledoc false
+
+  use KotkowoWeb, :live_view
+
+  import KotkowoWeb.Components.Steps
+end
--- lib/kotkowo_web/router.ex
+++ lib/kotkowo_web/router.ex
@@ -17,25 +17,14 @@ defmodule KotkowoWeb.Router do
   scope "/", KotkowoWeb do
     pipe_through :browser
 
-    get "/", PageController, :home
+    live "/", HomeLive.Index
 
     scope "/adopcja" do
       live "/szukaja-domu/:slug", AdoptionLive.Show
     end
   end
 
-  # Other scopes may use custom stacks.
-  # scope "/api", KotkowoWeb do
-  #   pipe_through :api
-  # end
-
-  # Enable LiveDashboard and Swoosh mailbox preview in development
   if Application.compile_env(:kotkowo, :dev_routes) do
-    # If you want to use the LiveDashboard in production, you should put
-    # it behind authentication and allow only admins to access it.
-    # If your application does not have an admins-only section yet,
-    # you can use Plug.BasicAuth to set up some basic authentication
-    # as long as you are also using SSL (which you should anyway).
     import Phoenix.LiveDashboard.Router
 
     scope "/dev" do
