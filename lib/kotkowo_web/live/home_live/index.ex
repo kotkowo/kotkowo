17f689aee8bb85a91ea61aec91efc68f476079fa
RustySnek <jodlowskipascal@gmail.com>
2023-09-22 09:20:09 +0200

    :coffin: Substitute placeholders with graphql announcements

--- lib/kotkowo_web/live/home_live/index.ex
+++ lib/kotkowo_web/live/home_live/index.ex
@@ -4,4 +4,14 @@ defmodule KotkowoWeb.HomeLive.Index do
   use KotkowoWeb, :live_view
 
   import KotkowoWeb.Components.Steps
+
+  alias Kotkowo.StrapiClient
+
+  @impl true
+  def mount(_params, _session, socket) do
+    {:ok, news} = StrapiClient.list_announcements(3)
+    socket = assign(socket, :news, news)
+
+    {:ok, socket}
+  end
 end
