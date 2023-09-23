836aa0b97d3f8d57b12e586c46c71aa25b400c52
Maksymilian Jodłowski <maksymilian.jodlowski@gmail.com>
2023-09-23 09:13:43 +0200

    :bug: Fix type issues

--- assets/js/app.ts
+++ assets/js/app.ts
@@ -1,44 +1,29 @@
-// If you want to use Phoenix channels, run `mix help phx.gen.channel`
-// to get started and then uncomment the line below.
-// import "./user_socket.js"
-
-// You can include dependencies in two ways.
-//
-// The simplest option is to put them in assets/vendor and
-// import them using relative paths:
-//
-//     import "../vendor/some-package.js"
-//
-// Alternatively, you can `npm install some-package --prefix assets` and import
-// them using a path starting with the package name:
-//
-//     import "some-package"
-//
-
-// Include phoenix_html to handle method=PUT/DELETE in forms and buttons.
 import 'phoenix_html'
-// Establish Phoenix Socket and LiveView configuration.
 import { Socket } from 'phoenix'
 import { LiveSocket } from 'phoenix_live_view'
 import topbar from '../vendor/topbar'
 import Alpine from 'alpinejs'
+import type { AlpineComponent } from 'alpinejs'
 import breakpoint from 'alpinejs-breakpoints'
 
-function toast() {
+function toast() : AlpineComponent<{
+	percentage: number,
+	show: () => void
+}> {
   return {
     percentage: 0,
     show() {
       this.percentage = 100
       if (this.interval !== undefined) clearInterval(this.interval)
 
-      this.interval = setInterval(async () => {
+      this.interval = setInterval((async () => {
         await this.$nextTick()
         this.percentage -= 1
         if (this.percentage <= 0) {
           clearInterval(this.interval)
           this.percentage = 0
         }
-      }, 10)
+      }) as () => void, 10)
     }
   }
 }
@@ -54,27 +39,21 @@ const liveSocket = new LiveSocket('/live', Socket, {
   params: { _csrf_token: csrfToken },
   dom: {
     onBeforeElUpdated(from, to) {
-      if (from._x_dataStack) {
-        window.Alpine.clone(from, to)
+      if (from._x_dataStack !== undefined) {
+        Alpine.clone(from, to)
       }
     }
   }
 })
 
-// Show progress bar on live navigation and form submits
 topbar.config({ barColors: { 0: '#29d' }, shadowColor: 'rgba(0, 0, 0, .3)' })
 window.addEventListener('phx:page-loading-start', _info =>
   topbar.delayedShow(200)
 )
 window.addEventListener('phx:page-loading-stop', _info => topbar.hide())
 
-// connect if there are any LiveViews on the page
 liveSocket.connect()
 
-// expose liveSocket on window for web console debug logs and latency simulation:
-// >> liveSocket.enableDebug()
-// >> liveSocket.enableLatencySim(1000)  // enabled for duration of browser session
-// >> liveSocket.disableLatencySim()
 window.liveSocket = liveSocket
 
 Alpine.plugin(breakpoint)
