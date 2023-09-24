499977c704e14c8d761d315607930a16b118628f
Maksymilian Jodłowski <maksymilian.jodlowski@gmail.com>
2023-09-24 18:03:17 +0200

    :bug: Fix strapi URL

--- config/runtime.exs
+++ config/runtime.exs
@@ -1,21 +1,13 @@
 import Config
 
-# config/runtime.exs is executed for all environments, including
-# during releases. It is executed after compilation and before the
-# system starts, so it is typically used to load production configuration
-# and secrets from environment variables or elsewhere. Do not define
-# any compile-time configuration in here, as it won't be applied.
-# The block below contains prod specific runtime configuration.
+strapi_endpoint =
+  System.get_env("STRAPI_ENDPOINT") ||
+    raise """
+    environment variable STRAPI_ENDPOINT is missing.
+    """
 
-# ## Using releases
-#
-# If you use `mix release`, you need to explicitly enable the server
-# by passing the PHX_SERVER=true when you start it:
-#
-#     PHX_SERVER=true bin/kotkowo start
-#
-# Alternatively, you can use `mix phx.gen.release` to generate a `bin/server`
-# script that automatically sets the env var above.
+config :kotkowo, :strapi, endpoint: strapi_endpoint
+
 if System.get_env("PHX_SERVER") do
   config :kotkowo, KotkowoWeb.Endpoint, server: true
 end
@@ -38,54 +30,4 @@ if config_env() == :prod do
       port: port
     ],
     secret_key_base: secret_key_base
-
-  # ## SSL Support
-  #
-  # To get SSL working, you will need to add the `https` key
-  # to your endpoint configuration:
-  #
-  #     config :kotkowo, KotkowoWeb.Endpoint,
-  #       https: [
-  #         ...,
-  #         port: 443,
-  #         cipher_suite: :strong,
-  #         keyfile: System.get_env("SOME_APP_SSL_KEY_PATH"),
-  #         certfile: System.get_env("SOME_APP_SSL_CERT_PATH")
-  #       ]
-  #
-  # The `cipher_suite` is set to `:strong` to support only the
-  # latest and more secure SSL ciphers. This means old browsers
-  # and clients may not be supported. You can set it to
-  # `:compatible` for wider support.
-  #
-  # `:keyfile` and `:certfile` expect an absolute path to the key
-  # and cert in disk or a relative path inside priv, for example
-  # "priv/ssl/server.key". For all supported SSL configuration
-  # options, see https://hexdocs.pm/plug/Plug.SSL.html#configure/1
-  #
-  # We also recommend setting `force_ssl` in your endpoint, ensuring
-  # no data is ever sent via http, always redirecting to https:
-  #
-  #     config :kotkowo, KotkowoWeb.Endpoint,
-  #       force_ssl: [hsts: true]
-  #
-  # Check `Plug.SSL` for all available options in `force_ssl`.
-
-  # ## Configuring the mailer
-  #
-  # In production you need to configure the mailer to use a different adapter.
-  # Also, you may need to configure the Swoosh API client of your choice if you
-  # are not using SMTP. Here is an example of the configuration:
-  #
-  #     config :kotkowo, Kotkowo.Mailer,
-  #       adapter: Swoosh.Adapters.Mailgun,
-  #       api_key: System.get_env("MAILGUN_API_KEY"),
-  #       domain: System.get_env("MAILGUN_DOMAIN")
-  #
-  # For this example you need include a HTTP client required by Swoosh API client.
-  # Swoosh supports Hackney and Finch out of the box:
-  #
-  #     config :swoosh, :api_client, Swoosh.ApiClient.Hackney
-  #
-  # See https://hexdocs.pm/swoosh/Swoosh.html#module-installation for details.
 end
--- lib/kotkowo/gallery_image.ex
+++ lib/kotkowo/gallery_image.ex
@@ -12,10 +12,21 @@ defmodule Kotkowo.GalleryImage do
   def url(gallery_image, size \\ :default)
 
   def url(%__MODULE__{} = gallery_image, :default) do
-    "http://localhost:1337/uploads/#{gallery_image.hash}#{gallery_image.ext}"
+    filename = "#{gallery_image.hash}#{gallery_image.ext}"
+    get_upload_url(filename)
   end
 
   def url(%__MODULE__{} = gallery_image, :thumbnail) do
-    "http://localhost:1337/uploads/thumbnail_#{gallery_image.hash}#{gallery_image.ext}"
+    filename = "thumbnail_#{gallery_image.hash}#{gallery_image.ext}"
+    get_upload_url(filename)
+  end
+
+  defp get_upload_url(filename) do
+    :kotkowo
+    |> Application.get_env(:strapi)
+    |> Keyword.get(:endpoint)
+    |> URI.parse()
+    |> Map.put(:path, "/uploads/#{filename}")
+    |> to_string()
   end
 end
