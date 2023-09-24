ca31973786e5652332a80228e362ef72205e40ac
RustySnek <jodlowskipascal@gmail.com>
2023-09-24 11:12:25 +0200

    :memo: Add specs for announcement and article

--- lib/kotkowo/article.ex
+++ lib/kotkowo/article.ex
@@ -1,5 +1,11 @@
 defmodule Kotkowo.Article do
   @moduledoc false
+
+  @type t :: %__MODULE__{
+          introduction: String.t(),
+          image: GalleryImage,
+          content: String.t()
+        }
   defstruct [
     :introduction,
     :image,
