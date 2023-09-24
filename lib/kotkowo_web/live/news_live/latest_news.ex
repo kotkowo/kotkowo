5a1eccf1de9d26c084fcc3b25c512be540d53c58
RustySnek <jodlowskipascal@gmail.com>
2023-09-24 11:18:13 +0200

    :adhesive_bandage: Graphql is hit only once now

--- lib/kotkowo_web/live/news_live/latest_news.ex
+++ lib/kotkowo_web/live/news_live/latest_news.ex
@@ -2,15 +2,26 @@ defmodule KotkowoWeb.NewsLive.LatestNews do
   @moduledoc false
   use KotkowoWeb, :live_view
 
+  import KotkowoWeb.Components.Static.HowYouCanHelpSection
+
   alias Kotkowo.GalleryImage
   alias Kotkowo.StrapiClient
 
   @impl true
   def mount(_params, _session, socket) do
-    {:ok, header_news} = StrapiClient.list_announcements(1)
-    {:ok, popular_news} = StrapiClient.list_announcements(2, 1)
-    {:ok, news} = StrapiClient.list_announcements(4, 3)
-    {:ok, article} = StrapiClient.get_article(Integer.to_string(hd(header_news).id))
+    {:ok, [header_news | tail]} = StrapiClient.list_announcements(7)
+    popular_news = Enum.take(tail, 2)
+
+    news =
+      tail
+      |> Enum.drop(2)
+      |> Enum.take(4)
+
+    {:ok, article} =
+      header_news
+      |> Map.get(:id)
+      |> Integer.to_string()
+      |> StrapiClient.get_article_for_announcement()
 
     socket =
       socket
@@ -18,7 +29,7 @@ defmodule KotkowoWeb.NewsLive.LatestNews do
       |> assign(:header_news, header_news)
       |> assign(:article, article)
       |> assign(:popular_news, popular_news)
-      |> assign(:popular_titles, header_news ++ popular_news)
+      |> assign(:popular_titles, [header_news | popular_news])
 
     {:ok, socket}
   end
