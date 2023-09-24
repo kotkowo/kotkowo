018b88ce5ea0e9f1805a23822ba5945f6116b94d
RustySnek <jodlowskipascal@gmail.com>
2023-09-24 11:12:25 +0200

    :memo: Add specs for announcement and article

--- lib/kotkowo/announcement.ex
+++ lib/kotkowo/announcement.ex
@@ -1,4 +1,12 @@
 defmodule Kotkowo.Announcement do
   @moduledoc false
+  alias Kotkowo.GalleryImage
+
+  @type t :: %__MODULE__{
+          title: String.t(),
+          image: GalleryImage,
+          tags: [String.t()],
+          id: pos_integer()
+        }
   defstruct [:title, :image, :tags, :id]
 end
