b03dc382bfba51469cd3e505313e58e11cbf00b0
Maksymilian Jodłowski <maksymilian.jodlowski@gmail.com>
2023-10-08 15:03:53 +0200

    :bug: Ignore false positive from gradient

--- .gradient_ignore.exs
+++ .gradient_ignore.exs
@@ -1,5 +1,10 @@
 [
   "lib/kotkowo_web/endpoint.ex",
   "lib/kotkowo/mailer.ex",
-  "lib/kotkowo/repo.ex"
+  "lib/kotkowo/repo.ex",
+  # lib/kotkowo/gallery_image.ex: Illegal map type t() on line 0
+  # seems to be an issue with Elixir's URI module:
+  # ** (RuntimeError) Unrecognized ignore line: 
+  # {"lib/kotkowo/gallery_image.ex", {:illegal_map_type, {:user_type, [file: ~c"Elixir.URI.erl", location: 0], :t, []}}}
+  {"lib/kotkowo/gallery_image.ex", :illegal_map_type}
 ]
