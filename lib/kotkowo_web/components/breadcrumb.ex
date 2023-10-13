556bc784a9ed92e9d9a1f10bb22157b14d68dab7
Maksymilian Jodłowski <maksymilian.jodlowski@gmail.com>
2023-10-13 22:55:45 +0200

    :bread: Add new breadcrumb component

---
+++ lib/kotkowo_web/components/breadcrumb.ex
@@ -0,0 +1,27 @@
+defmodule KotkowoWeb.Components.Breadcrumb do
+  @moduledoc false
+  use KotkowoWeb, :component
+
+  slot :crumb do
+    attr :navigate, :string
+    attr :active, :boolean
+  end
+
+  def breadcrumb(assigns) do
+    ~H"""
+    <div class="gap-x-2 text-sm pb-8 pt-10 items-center hidden xl:flex">
+      <div>
+        <.link navigate={~p"/"}>
+          Strona główna
+        </.link>
+      </div>
+      <div :for={crumb <- @crumb} class="flex gap-x-2 items-center">
+        <span class="mb-0.5">&gt;</span>
+        <.link navigate={crumb[:navigate]} class={[crumb[:active] && "font-bold pointer-events-none"]}>
+          <%= render_slot(crumb) %>
+        </.link>
+      </div>
+    </div>
+    """
+  end
+end
