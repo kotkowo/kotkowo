d201e8570de35c516f35f317deaf24a0cb1e188f
RustySnek <jodlowskipascal@gmail.com>
2023-10-28 22:09:13 +0200

    subject to change

---
+++ lib/kotkowo_web/live/news_live/found_home.ex
@@ -0,0 +1,13 @@
+defmodule KotkowoWeb.NewsLive.FoundHome do
+  @moduledoc false
+  use KotkowoWeb, :live_view
+
+  import KotkowoWeb.Components.Static.HowYouCanHelpSection
+
+
+  @impl true
+  def mount(_params, _session, socket) do
+
+    {:ok, socket}
+  end
+end
