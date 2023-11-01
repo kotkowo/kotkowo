6080f4e54bc52f58753899ab695f2a6b136d9069
Pascal Jodlowski <jodlowskipascal@gmail.com>
2023-11-01 22:12:58 +0100

    :sparkles: Add adopted_cat model

---
+++ lib/kotkowo/adopted_cat.ex
@@ -0,0 +1,10 @@
+defmodule Kotkowo.AdoptedCat do
+  @moduledoc false
+  alias Kotkowo.Cat
+  defstruct [:cat, :adoption_date, :id]
+  @type t :: %__MODULE__{
+          id: pos_integer(),
+        cat: Cat
+        }
+
+end
