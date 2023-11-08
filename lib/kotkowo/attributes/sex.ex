5ae0568e84ecd8c6ae0f201b3eefa255cf5e10a0
Pascal Jodlowski <jodlowskipascal@gmail.com>
2023-11-08 22:20:28 +0100

    :sparkles: Add appropriate gettext to genders

--- lib/kotkowo/attributes/sex.ex
+++ lib/kotkowo/attributes/sex.ex
@@ -1,8 +1,9 @@
 defmodule Kotkowo.Attributes.Sex do
   @moduledoc false
+  import KotkowoWeb.Gettext
 
   def all, do: [:male, :female]
 
-  def to_string(:male), do: raise(UndefinedFunctionError)
-  def to_string(:female), do: raise(UndefinedFunctionError)
+  def to_string(:male), do: gettext("Kocur")
+  def to_string(:female), do: gettext("Kotka")
 end
