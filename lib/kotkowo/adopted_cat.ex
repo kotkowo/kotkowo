801429e7418aab66a297b8afc0ffaf5900eea911
Pascal Jodlowski <jodlowskipascal@gmail.com>
2023-11-14 11:13:51 +0100

    :art: Format code

--- lib/kotkowo/adopted_cat.ex
+++ lib/kotkowo/adopted_cat.ex
@@ -1,10 +1,11 @@
 defmodule Kotkowo.AdoptedCat do
   @moduledoc false
   alias Kotkowo.Cat
+
   defstruct [:cat, :adoption_date, :id]
+
   @type t :: %__MODULE__{
           id: pos_integer(),
-        cat: Cat
+          cat: Cat
         }
-
 end
