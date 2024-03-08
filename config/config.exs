7291514998e2d5d1d16fca5f48d7cbaeac63678e
Maksymilian Jodłowski <maksymilian.jodlowski@gmail.com>
2024-03-08 01:53:40 +0100

    🐛 Fix Sentry env that should be in runtime

--- config/config.exs
+++ config/config.exs
@@ -51,13 +51,6 @@ config :phoenix, :json_library, Jason
 
 config :gettext, default_locale: "pl"
 
-config :sentry,
-  dsn: System.get_env("SENTRY_DSN"),
-  environment_name: Mix.env(),
-  included_environments: [:prod],
-  enable_source_code_context: true,
-  root_source_code_paths: [File.cwd!()]
-
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{config_env()}.exs"
