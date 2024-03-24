7005846c1c12f5fc0e8730615c638c72c0a6fa45
Maksymilian Jodłowski <maksymilian.jodlowski@gmail.com>
2024-03-24 07:27:30 +0100

    ❄️ Fix elixir-ls and disable next-ls

--- flake.nix
+++ flake.nix
@@ -1,27 +1,51 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     devenv.url = "github:cachix/devenv";
+
     fenix.url = "github:nix-community/fenix";
     fenix.inputs = {nixpkgs.follows = "nixpkgs";};
+
+    next-ls.url = "github:elixir-tools/next-ls";
   };
 
   nixConfig = {
-    extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
-    extra-substituters = "https://devenv.cachix.org";
+    extra-trusted-public-keys = [
+      "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
+      "elixir-tools.cachix.org-1:GfK9E139Ysi+YWeS1oNN9OaTfQjqpLwlBaz+/73tBjU="
+    ];
+    extra-substituters = [
+      "https://devenv.cachix.org"
+      "https://elixir-tools.cachix.org"
+    ];
   };
 
   outputs = {
     self,
     nixpkgs,
     devenv,
+    next-ls,
     ...
   } @ inputs: let
     pkgs = nixpkgs.legacyPackages."x86_64-linux";
     erlang = pkgs.beam.packages.erlangR26;
-    elixir = erlang.elixir_1_15;
+    elixir = erlang.elixir_1_16;
     node = pkgs.nodejs_20;
-    elixir-ls = erlang.elixir-ls.override {elixir = elixir;};
+    elixir-ls = (erlang.elixir-ls.override {elixir = elixir;}).overrideAttrs (prev: {
+      version = "master";
+      src = pkgs.fetchFromGitHub {
+        owner = "elixir-lsp";
+        repo = "elixir-ls";
+        rev = "06178f5cc5babad9522ba1dc67046ffc48a0d6ed";
+        hash = "sha256-8tFQ68gxlHjzCfgdB5QFaxMoelN5Ht4XmqtuM8Fwg2E=";
+        fetchSubmodules = true;
+      };
+      buildPhase = ''
+        runHook preBuild
+        mix do compile --no-deps-check, elixir_ls.release2
+        runHook postBuild
+      '';
+    });
   in {
     devShell.x86_64-linux = devenv.lib.mkShell {
       inherit inputs pkgs;
@@ -50,10 +74,10 @@
             [
               docker
               node
-
               nodePackages."@tailwindcss/language-server"
               nodePackages.vscode-html-languageserver-bin
-              elixir_ls
+              elixir-ls
+              # next-ls.packages.x86_64-linux.default
               openssl
               graphql-client
             ]
