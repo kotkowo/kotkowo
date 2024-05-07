bbeb5c9105649933c035357f09b008b1eabd3d0d
Pascal Jodlowski <jodlowskipascal@gmail.com>
2024-05-07 15:25:46 +0200

    :sparkles: add event listeners for showing and closing share modal

--- assets/js/app.ts
+++ assets/js/app.ts
@@ -51,6 +51,42 @@ window.addEventListener('phx:page-loading-start', _info =>
   topbar.delayedShow(200)
 )
 window.addEventListener('phx:page-loading-stop', _info => topbar.hide())
+window.addEventListener('share_modal', (e) => {
+  const elem = e.target;
+  const href = elem.getAttribute("share_href");
+  const quote = elem.getAttribute("share_quote");
+
+  const open_share_modal = () => {
+    console.warn('Web Share Api is not supported by the current browser.');
+    let share_dialog = document.getElementById("share-dialog")
+    share_dialog.showModal()
+    let facebook_share = document.getElementById("share-facebook")
+    facebook_share.setAttribute("href", `https://www.facebook.com/sharer/sharer.php?u=${href}&hashtag=kotkowo`)
+    let twitter_share = document.getElementById("share-twitter")
+    twitter_share.setAttribute("href", `https://twitter.com/intent/tweet?url=${href}&text=${quote}`)
+    let share_copy = document.getElementById("share-copy")
+    share_copy.value = href
+  }
+
+  if (navigator.share) {
+    navigator
+    .share({
+        href,
+        quote
+      })
+    .catch((error) => {
+      open_share_modal()
+    })          
+  } else {
+    open_share_modal()
+  }
+})
+
+window.addEventListener("hide_share", (e) => {
+  let share_dialog = document.getElementById("share-dialog")
+  share_dialog.close();
+  
+})
 
 liveSocket.connect()
 
