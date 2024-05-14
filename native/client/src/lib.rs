e0d1d2ff7da754f975e42a36a1c5bb1db976cf56
Pascal Jodlowski <jodlowskipascal@gmail.com>
2024-05-14 10:23:12 +0200

    :sparkles: add list_announcement in client

--- native/client/src/lib.rs
+++ native/client/src/lib.rs
@@ -1,8 +1,16 @@
-use kotkowo_client::{list_cat, Cat, CatFilter, Error, Options, Paged};
+use kotkowo_client::{
+    list_announcement, list_cat, Announcement, AnnouncementFilter, Cat, CatFilter, Error, Options,
+    Paged,
+};
 
 #[rustler::nif]
 fn list_cats(options: Options<CatFilter>) -> Result<Paged<Cat>, Error> {
     list_cat(options)
 }
 
-rustler::init!("Elixir.Kotkowo.Client", [list_cats,]);
+#[rustler::nif]
+fn list_announcements(options: Options<AnnouncementFilter>) -> Result<Paged<Announcement>, Error> {
+    list_announcement(options)
+}
+
+rustler::init!("Elixir.Kotkowo.Client", [list_cats, list_announcements]);
