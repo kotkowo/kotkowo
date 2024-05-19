873a324c668f465114f72a91fe4b5c294de5bacb
Pascal Jodlowski <jodlowskipascal@gmail.com>
2024-05-19 23:10:44 +0200

    :sparkles: Add articles to client

---
+++ lib/kotkowo/client/article.ex
@@ -0,0 +1,21 @@
+defmodule Kotkowo.Client.Article do
+  @moduledoc false
+
+  alias Kotkowo.Client.Image
+
+  @type tags() :: [String.t()]
+
+  defstruct [
+    :id,
+    :introduction,
+    :content,
+    :image
+  ]
+
+  @type t() :: %__MODULE__{
+          id: String.t() | nil,
+          introduction: String.t(),
+          content: String.t(),
+          image: Image.t() | nil
+        }
+end
