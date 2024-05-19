47fd9179ae417f4dd5e93f86043718fa01a6afc0
Pascal Jodlowski <jodlowskipascal@gmail.com>
2024-05-19 23:14:22 +0200

    :zap: new client here

--- lib/kotkowo_web/live/news_live/announcements/latest_news.ex
+++ lib/kotkowo_web/live/news_live/announcements/latest_news.ex
@@ -4,42 +4,76 @@ defmodule KotkowoWeb.AnnouncementsLive.LatestNews do
 
   import KotkowoWeb.Components.Static.HowYouCanHelpSection
 
-  alias Kotkowo.Article
   alias Kotkowo.Client
   alias Kotkowo.Client.Announcement
+  alias Kotkowo.Client.Article
   alias Kotkowo.Client.Paged
-  alias Kotkowo.StrapiClient
+
+  require Logger
+
+  @impl true
+  def handle_async(:load_article, {:ok, article}, socket) do
+    socket =
+      case article do
+        :error -> assign(socket, :article, :error)
+        article -> assign(socket, :article, article)
+      end
+
+    {:noreply, socket}
+  end
+
+  @impl true
+  def handle_async(:load_announcements, {:ok, announcements}, socket) do
+    socket =
+      case announcements do
+        {:ok, %Paged{items: news}} ->
+          {header_news, news} = List.pop_at(news, 0)
+          {popular_news, news} = Enum.split(news, 2)
+
+          news =
+            Enum.take(news, 4)
+
+          socket
+          |> assign(:news, news)
+          |> assign(:header_news, header_news)
+          |> assign(:popular_news, popular_news)
+          |> assign(:popular_titles, [header_news | popular_news])
+          |> assign_async(:article, fn -> {:ok, %{article: get_article(header_news)}} end)
+
+        {:error, message} ->
+          Logger.error(message)
+          socket
+      end
+
+    {:noreply, socket}
+  end
 
   @impl true
   def mount(_params, _session, socket) do
-    {:ok, %Paged{items: news, page_count: _page_count, page_size: _page_size, page: _page, total: _total}} =
-      [page: 0, page_size: 7, filter: nil] |> Client.new() |> Client.list_announcements()
-
-    {header_news, news} = List.pop_at(news, 0)
-    {popular_news, news} = Enum.split(news, 2)
-
-    news =
-      Enum.take(news, 4)
-
-    article = get_article(header_news)
-
     socket =
       socket
-      |> assign(:article, article)
-      |> assign(:news, news)
-      |> assign(:header_news, header_news)
-      |> assign(:popular_news, popular_news)
-      |> assign(:popular_titles, [header_news | popular_news])
+      |> assign(:header_news, nil)
+      |> assign(:popular_news, nil)
+      |> assign(:popular_titles, nil)
+      |> assign(:news, nil)
+      |> assign(:article, nil)
+      |> start_async(:load_announcements, fn ->
+        [page: 0, page_size: 7] |> Client.new() |> Client.list_announcements()
+      end)
 
     {:ok, socket}
   end
 
-  defp get_article(nil), do: nil
+  defp get_article(nil), do: :error
 
   defp get_article(%Announcement{} = news) do
-    case StrapiClient.get_article_for_announcement(news.id) do
-      {:ok, %Article{} = article} -> article
-      _ -> nil
+    case Client.get_article(news.id) do
+      {:ok, %Article{} = article} ->
+        article
+
+      {:error, message} ->
+        Logger.error(message)
+        :error
     end
   end
 end
