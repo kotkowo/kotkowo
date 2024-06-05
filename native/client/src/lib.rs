e07af9173811d6f1bb66a3fbd74fa69655a7dc7a
Pascal Jodlowski <jodlowskipascal@gmail.com>
2024-06-05 22:19:33 +0200

    :crab: add client adopted_cats

--- native/client/src/lib.rs
+++ native/client/src/lib.rs
@@ -1,6 +1,7 @@
 use kotkowo_client::{
-    get_announcement_article, list_announcement, list_cat, Announcement, AnnouncementFilter,
-    Article, Cat, CatFilter, Error, Options, Paged,
+    get_announcement_article, list_adopted_cat, list_announcement, list_cat, AdoptedCat,
+    Announcement, AnnouncementFilter, Article, BetweenDateTime, Cat, CatFilter, Error, Options,
+    Paged,
 };
 
 #[rustler::nif]
@@ -18,7 +19,20 @@ fn get_article(announcement_id: String) -> Result<Article, Error> {
     get_announcement_article(announcement_id)
 }
 
+#[rustler::nif]
+fn list_adopted_cats(
+    options: Options<CatFilter>,
+    between_datetime: Option<BetweenDateTime>,
+) -> Result<Paged<AdoptedCat>, Error> {
+    list_adopted_cat(options, between_datetime)
+}
+
 rustler::init!(
     "Elixir.Kotkowo.Client",
-    [list_cats, list_announcements, get_article]
+    [
+        list_cats,
+        list_announcements,
+        get_article,
+        list_adopted_cats
+    ]
 );
