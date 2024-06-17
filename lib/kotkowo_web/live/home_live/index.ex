f8c716783acb9f893d1be1c4bbf3af6ad2b0ff01
Pascal Jodlowski <jodlowskipascal@gmail.com>
2024-06-17 16:09:18 +0200

    :ambulance: fix landing page

--- lib/kotkowo_web/live/home_live/index.ex
+++ lib/kotkowo_web/live/home_live/index.ex
@@ -5,19 +5,35 @@ defmodule KotkowoWeb.HomeLive.Index do
 
   import KotkowoWeb.Components.Steps
   import KotkowoWeb.Constants, only: [kotkowo_mail: 0]
+  import KotkowoWeb.WebHelpers
 
   alias Kotkowo.Client
+  alias Kotkowo.Client.Cat
   alias Kotkowo.Client.Paged
-  alias Kotkowo.GalleryImage
-  alias Kotkowo.StrapiClient
 
   require Logger
 
   @impl true
-  def handle_info({:load_announcements}, socket) do
-    client = Client.new(page: 1, page_size: 3)
+  def handle_async(:load_cats, {:ok, cats}, socket) do
+    case cats do
+      {:ok, %Paged{items: cats}} ->
+        {:noreply, assign(socket, :in_need_of_a_new_home, cats)}
 
-    case Client.list_announcements(client) do
+      {:error, message} ->
+        Logger.error(message)
+
+        socket =
+          socket
+          |> put_flash(:error, message)
+          |> assign(:in_need_of_a_new_home, :error)
+
+        {:noreply, socket}
+    end
+  end
+
+  @impl true
+  def handle_async(:load_announcements, {:ok, announcements}, socket) do
+    case announcements do
       {:ok, %Paged{items: news}} ->
         {:noreply, assign(socket, :news, news)}
 
@@ -35,22 +51,22 @@ defmodule KotkowoWeb.HomeLive.Index do
 
   @impl true
   def mount(_params, _session, socket) do
-    if connected?(socket), do: send(self(), {:load_announcements})
+    default_opts = [page: 1, page_size: 3]
 
-    in_need_of_a_new_home =
-      case StrapiClient.list_adopted_cats(3) do
-        {:ok, result} ->
-          result
-
-        {:error, {_reason, message}} ->
-          Logger.error(message)
-          :error
-      end
+    non_dead_or_adopted_filter =
+      Cat.Filter.from_map(%{
+        is_dead: false,
+        include_adopted: false
+      })
 
     socket =
       socket
       |> assign(:news, [])
-      |> assign(:in_need_of_a_new_home, in_need_of_a_new_home)
+      |> assign(:in_need_of_a_new_home, [])
+      |> start_async(:load_announcements, fn -> default_opts |> Client.new() |> Client.list_announcements() end)
+      |> start_async(:load_cats, fn ->
+        default_opts |> Keyword.put(:filter, non_dead_or_adopted_filter) |> Client.new() |> Client.list_cats()
+      end)
 
     {:ok, socket}
   end
