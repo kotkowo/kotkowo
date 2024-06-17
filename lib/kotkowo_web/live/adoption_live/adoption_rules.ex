d9d647afc5eb14bb5abf12362020407d8b708810
Pascal Jodlowski <jodlowskipascal@gmail.com>
2024-06-17 21:55:29 +0200

    :alien: fix adoption rules

--- lib/kotkowo_web/live/adoption_live/adoption_rules.ex
+++ lib/kotkowo_web/live/adoption_live/adoption_rules.ex
@@ -5,12 +5,37 @@ defmodule KotkowoWeb.AdoptionLive.AdoptionRules do
   import KotkowoWeb.Components.Static.HowYouCanHelpSection
   import KotkowoWeb.Components.Steps
 
-  alias Kotkowo.GalleryImage
-  alias Kotkowo.StrapiClient
+  alias Kotkowo.Client
+  alias Kotkowo.Client.Cat
+  alias Kotkowo.Client.Image
+  alias Kotkowo.Client.Paged
+
+  require Logger
+
+  def handle_async(:load_cats, {:ok, cats}, socket) do
+    socket =
+      case cats do
+        {:ok, %Paged{items: cats}} ->
+          assign(socket, :in_need_of_a_new_home, cats)
+
+        {:error, message} ->
+          Logger.error(message)
+          assign(socket, :in_need_of_a_new_home, :error)
+      end
+
+    {:noreply, socket}
+  end
 
   def mount(_params, _session, socket) do
-    {:ok, in_need_of_a_new_home} = StrapiClient.list_adopted_cats(3)
-    socket = assign(socket, :in_need_of_a_new_home, in_need_of_a_new_home)
+    default_filter = %{} |> Cat.Filter.from_map() |> Map.put(:include_adopted, false) |> Map.put(:is_dead, false)
+
+    socket =
+      socket
+      |> assign(:in_need_of_a_new_home, [])
+      |> start_async(:load_cats, fn ->
+        [page: 1, page_size: 3, filter: default_filter] |> Client.new() |> Client.list_cats()
+      end)
+
     {:ok, socket}
   end
 end
