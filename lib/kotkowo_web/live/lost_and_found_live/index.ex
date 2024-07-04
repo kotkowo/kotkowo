fb5c33a46f9db13678c8ffcc7866ca9f18f6a23f
Pascal Jodlowski <jodlowskipascal@gmail.com>
2024-07-05 00:42:51 +0200

    :sparkles: lost and found list_cat pages

--- lib/kotkowo_web/live/lost_and_found_live/index.ex
+++ lib/kotkowo_web/live/lost_and_found_live/index.ex
@@ -5,4 +5,128 @@ defmodule KotkowoWeb.LostAndFoundLive.Index do
   import KotkowoWeb.Components.Static.HowYouCanHelpSection
   import KotkowoWeb.Components.Steps
   import KotkowoWeb.Constants
+  import KotkowoWeb.WebHelpers
+
+  alias Kotkowo.Client
+  alias Kotkowo.Client.Cat
+  alias Kotkowo.Client.Image
+  alias Kotkowo.Client.Paged
+
+  require Logger
+
+  @impl true
+  def handle_async(:load_found_cats, {:ok, cats}, socket) do
+    socket =
+      case cats do
+        {:ok, %Paged{items: cats}} ->
+          assign(socket, :found_cats, cats)
+
+        {:error, msg} ->
+          Logger.error(msg)
+          socket
+      end
+
+    {:noreply, socket}
+  end
+
+  @impl true
+  def handle_async(:load_lost_cats, {:ok, cats}, socket) do
+    socket =
+      case cats do
+        {:ok, %Paged{items: cats}} ->
+          assign(socket, :lost_cats, cats)
+
+        {:error, msg} ->
+          Logger.error(msg)
+          socket
+      end
+
+    {:noreply, socket}
+  end
+
+  @impl true
+  def handle_event("query_lost", %{"chip_number" => chip_number}, socket) do
+    chip_number = unless chip_number == "", do: chip_number
+    raw_lost_filter = Map.put(socket.assigns.raw_lost_filter, :chip_number, chip_number)
+    filter = Cat.Filter.from_map(raw_lost_filter)
+
+    params =
+      Cat.Filter.to_params(filter, "lost_cat")
+
+    socket =
+      socket
+      |> assign(:raw_lost_filter, raw_lost_filter)
+      |> start_async(:load_lost_cats, fn -> [filter: filter] |> Client.new() |> Client.list_lost_cats() end)
+      |> push_patch(
+        to:
+          ~p"/zaginione-znalezione" <>
+            "?#{params}" <>
+            "#{socket.assigns.raw_found_filter |> Cat.Filter.from_map() |> Cat.Filter.to_params("found_cat")}"
+      )
+
+    {:noreply, socket}
+  end
+
+  @impl true
+  def handle_event("query_seen_found", %{"chip_number" => chip_number}, socket) do
+    chip_number = unless chip_number == "", do: chip_number
+    raw_found_filter = Map.put(socket.assigns.raw_found_filter, :chip_number, chip_number)
+
+    filter =
+      Cat.Filter.from_map(raw_found_filter)
+
+    params = Cat.Filter.to_params(filter, "found_cat")
+
+    socket =
+      socket
+      |> assign(:raw_found_filter, raw_found_filter)
+      |> start_async(:load_found_cats, fn -> [filter: filter] |> Client.new() |> Client.list_found_cats() end)
+      |> push_patch(
+        to:
+          ~p"/zaginione-znalezione" <>
+            "?#{params}" <>
+            "#{socket.assigns.raw_lost_filter |> Cat.Filter.from_map() |> Cat.Filter.to_params("lost_cat")}"
+      )
+
+    {:noreply, socket}
+  end
+
+  @impl true
+  def handle_params(params, _url, socket) do
+    lost_filter = Cat.Filter.from_params(params["lost_cat"])
+    found_filter = Cat.Filter.from_params(params["found_cat"])
+
+    socket =
+      socket
+      |> start_async(:load_found_cats, fn -> [filter: found_filter] |> Client.new() |> Client.list_found_cats() end)
+      |> start_async(:load_lost_cats, fn -> [filter: lost_filter] |> Client.new() |> Client.list_lost_cats() end)
+
+    {:noreply, socket}
+  end
+
+  @impl true
+  def mount(params, _session, socket) do
+    lost_filter = Cat.Filter.from_params(params["lost_cat"])
+    found_filter = Cat.Filter.from_params(params["found_cat"])
+    raw_lost_filter = sanitize_filter_to_map(lost_filter)
+    raw_found_filter = sanitize_filter_to_map(found_filter)
+
+    socket =
+      socket
+      |> assign(:raw_found_filter, raw_found_filter)
+      |> assign(:raw_lost_filter, raw_lost_filter)
+      |> assign(:found_cats, nil)
+      |> assign(:lost_cats, nil)
+
+    {:ok, socket}
+  end
+
+  defp sanitize_filter_to_map(filter) do
+    filter
+    |> Map.from_struct()
+    |> Map.new(fn
+      {k, {_, v}} -> {k, v}
+      {k, v} -> {k, v}
+    end)
+  end
 end
--- lib/kotkowo_web/live/lost_and_found_live/lost/index.ex
+++ lib/kotkowo_web/live/lost_and_found_live/lost/index.ex
@@ -1,3 +1,137 @@
 defmodule KotkowoWeb.LostAndFoundLive.LostLive.Index do
+  @moduledoc false
   use KotkowoWeb, :live_view
+
+  import KotkowoWeb.Components.Static.HowYouCanHelpSection
+  import KotkowoWeb.Constants
+  import KotkowoWeb.WebHelpers
+
+  alias Kotkowo.Client
+  alias Kotkowo.Client.Cat
+  alias Kotkowo.Client.Image
+  alias Kotkowo.Client.Paged
+
+  defp parse_int_param(nil), do: nil
+  defp parse_int_param(page) when is_integer(page), do: page
+
+  defp parse_int_param(page) when is_binary(page) do
+    case Integer.parse(page) do
+      {page, _rest} -> page
+      _ -> nil
+    end
+  end
+
+  @impl true
+  def mount(params, _session, socket) do
+    initial_filter = Cat.Filter.from_params(params["cat"])
+    socket = assign(socket, :initial_filter, initial_filter)
+    {:ok, socket}
+  end
+
+  @impl true
+  def handle_params(params, _uri, socket) do
+    filter = Cat.Filter.from_params(params["cat"])
+
+    page_size = params |> Map.get("page_size", "30") |> parse_int_param()
+    page = params |> Map.get("page") |> parse_int_param()
+
+    {:ok, %Paged{items: cats, page_count: page_count, page_size: page_size, page: page, total: total}} =
+      [page: page, page_size: page_size, filter: filter] |> Client.new() |> Client.list_lost_cats()
+
+    params = %{page: page, page_size: page_size}
+    page_count = max(1, page_count)
+
+    socket =
+      if page <= page_count do
+        socket
+        |> stream(:cats, cats, reset: true)
+        |> assign(:cats_total, total)
+        |> assign(:page_count, page_count)
+        |> assign(:params, params)
+      else
+        push_navigate(socket, to: ~p"/zaginione-znalezione/zaginione")
+      end
+
+    {:noreply, socket}
+  end
+
+  @impl true
+  def handle_info({:filter_cat, %Cat.Filter{} = filter}, socket) do
+    {:noreply, push_patch(socket, to: ~p"/zaginione-znalezione/zaginione" <> "?#{Cat.Filter.to_params(filter)}")}
+  end
+
+  defp items_per_page, do: [30, 60, 90]
+
+  def pagination_bar(assigns) do
+    assigns =
+      assigns
+      |> assign(:previous_page, assigns.selected_page - 1)
+      |> assign(:next_page, assigns.selected_page + 1)
+      |> assign(:first_page, 1)
+
+    ~H"""
+    <div class="flex flex-row text-xl gap-x-4">
+      <button
+        :if={@selected_page != @first_page}
+        phx-click="select_page"
+        class="p-4"
+        value={@selected_page - 1}
+      >
+        <.icon class=" rotate-90 brightness-0" name="chevron_down" />
+      </button>
+
+      <.icon :if={@selected_page == @first_page} class="rotate-90 p-4 grayscale" name="chevron_down" />
+      <div class="self-center gap-4 hidden lg:inline">
+        <button :if={@selected_page > 2} class="w-14 h-14" phx-click="select_page" value={@first_page}>
+          <%= @first_page %>
+        </button>
+        <span :if={@selected_page > 2} class="w-14 h-14">...</span>
+        <button
+          :if={@selected_page != @first_page}
+          class="w-14 h-14"
+          phx-click="select_page"
+          value={@previous_page}
+        >
+          <%= @previous_page %>
+        </button>
+        <button class="font-bold w-14 h-14">
+          <%= @selected_page %>
+        </button>
+        <button
+          :if={@selected_page != @last_page}
+          class="w-14 h-14"
+          phx-click="select_page"
+          value={@next_page}
+        >
+          <%= @next_page %>
+        </button>
+        <span :if={@selected_page < @last_page - 1} class="w-14 h-14">...</span>
+
+        <button
+          :if={@selected_page < @last_page - 1}
+          class="w-14 h-14"
+          phx-click="select_page"
+          value={@last_page}
+        >
+          <%= @last_page %>
+        </button>
+      </div>
+      <div class="self-center flex flex-row lg:hidden gap-4">
+        <span><%= @selected_page %></span>
+        <span>z</span>
+        <span><%= @last_page %></span>
+      </div>
+      <.icon :if={@selected_page == @last_page} class="rotate-90 p-4 grayscale" name="chevron_up" />
+
+      <button
+        :if={@selected_page != @last_page}
+        class="p-4"
+        phx-click="select_page"
+        value={@selected_page + 1}
+      >
+        <.icon class="rotate-90 brightness-0" name="chevron_up" />
+      </button>
+    </div>
+    """
+  end
 end
