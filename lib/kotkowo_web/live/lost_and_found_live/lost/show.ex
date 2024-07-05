075f1d2734e12ec288c2dc6a023c7b50d9775d20
Pascal Jodlowski <jodlowskipascal@gmail.com>
2024-07-05 12:35:02 +0200

    :sparkles: view pages for lost and found

--- lib/kotkowo_web/live/lost_and_found_live/lost/show.ex
+++ lib/kotkowo_web/live/lost_and_found_live/lost/show.ex
@@ -1,4 +1,53 @@
 defmodule KotkowoWeb.LostAndFoundLive.LostLive.Show do
   @moduledoc false
   use KotkowoWeb, :live_view
+
+  import KotkowoWeb.Components.CatViewUtils
+  import KotkowoWeb.Constants
+
+  alias Kotkowo.Client
+  alias Kotkowo.Client.Cat
+  alias Kotkowo.Client.LostCat
+  alias KotkowoWeb.Components.CatGallery
+
+  require Logger
+
+  def handle_async(:load_cat, {:ok, cat}, socket) do
+    case cat do
+      {:ok, %LostCat{} = cat} ->
+        images = cat.cat.images
+        {:noreply, socket |> assign(:cat, cat) |> assign(:images, images)}
+
+      {:error, message} ->
+        Logger.error(message)
+        {:noreply, socket}
+    end
+  end
+
+  def mount(%{"slug" => slug}, _session, socket) do
+    socket =
+      socket
+      |> assign(:cat, nil)
+      |> assign(:slug, slug)
+      |> assign(:images, [])
+      |> assign(:current_image, 0)
+      |> start_async(:load_cat, fn -> Client.get_lost_cat_by_slug(slug) end)
+
+    {:ok, socket}
+  end
+
+  def handle_params(params, url, socket) do
+    current_image =
+      case params |> Map.get("image", "0") |> Integer.parse() do
+        {n, _} -> n
+        _ -> 0
+      end
+
+    socket =
+      socket
+      |> assign(:path, url)
+      |> assign(:current_image, current_image)
+
+    {:noreply, socket}
+  end
 end
