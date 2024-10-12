69cb745583628c4f06947bd2d87e2fb0dacc207a
Pascal Jodlowski <jodlowskipascal@gmail.com>
2024-10-12 14:16:14 +0200

    :wrench: more constants

--- lib/kotkowo_web/constants.ex
+++ lib/kotkowo_web/constants.ex
@@ -11,4 +11,13 @@ defmodule KotkowoWeb.Constants do
   def cookie_policy, do: "#"
   def rodo_policy, do: "#"
   def privacy_policy, do: "#"
+  def krs, do: "0000345319"
+  def nip, do: "9662018446"
+  def regon, do: "20032337500000"
+  def swift_bic, do: "EBOSPLPWXXX"
+  def bank_account_number, do: "87 1540 1216 2054 4458 2306 0001"
+  def iban, do: "PL #{bank_account_number()}"
+  def bank, do: "BOŚ SA O. w Bialymstoku"
+  def kotkowo_address, do: "al. Piłsudskiego 26/31"
+  def postal_code, do: "15-446 Białystok"
 end
