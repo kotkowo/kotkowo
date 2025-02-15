13a0df5b1b5625303e529bb55aea7e339fc62060
Pascal Jodlowski <jodlowskipascal@gmail.com>
2025-02-15 22:52:37 +0100

    :construction: begin faq

---
+++ lib/kotkowo_web/live/about_us_live/faq.ex
@@ -0,0 +1,7 @@
+defmodule KotkowoWeb.AboutUsLive.Faq do 
+  use KotkowoWeb, :live_view
+
+  import KotkowoWeb.Components.Static.HowYouCanHelpSection
+  import KotkowoWeb.Constants
+  import KotkowoWeb.Components.Drawers
+end
