93c901044b184c2478e70547de700a4449c78020
Pascal Jodlowski <jodlowskipascal@gmail.com>
2025-02-15 09:35:56 +0100

    :ambulance: fix pagination

--- lib/kotkowo_web/live/adoption_live/looking_for_new_home.ex
+++ lib/kotkowo_web/live/adoption_live/looking_for_new_home.ex
@@ -2,6 +2,7 @@ defmodule KotkowoWeb.AdoptionLive.LookingForNewHome do
   @moduledoc false
   use KotkowoWeb, :live_view
 
+  import KotkowoWeb.Components.Pagination
   import KotkowoWeb.Components.Static.HowYouCanHelpSection
   import KotkowoWeb.Constants
 
@@ -11,6 +12,8 @@ defmodule KotkowoWeb.AdoptionLive.LookingForNewHome do
   alias Kotkowo.Client.Paged
 
   require Logger
+
+  @first_page 1
 
   defp parse_int_param(nil), do: nil
   defp parse_int_param(page) when is_integer(page), do: page
@@ -23,11 +26,82 @@ defmodule KotkowoWeb.AdoptionLive.LookingForNewHome do
   end
 
   @impl true
+  def handle_event("items_amount_owned", %{"items_per_page" => amount}, socket) do
+    limit = String.to_integer(amount)
+    params = %{socket.assigns.params | owned_limit: limit}
+
+    socket =
+      socket
+      |> assign(:params, params)
+      |> push_patch(
+        to:
+          ~p"/adopcja/szukaja-domu?#{params}" <>
+            "#{Cat.Filter.to_params(socket.assigns.owned_filter, "owned_cat")}" <>
+            "#{Cat.Filter.to_params(socket.assigns.unowned_filter, "unowned_cat")}",
+        replace: true
+      )
+
+    {:noreply, socket}
+  end
+
+  @impl true
+  def handle_event("items_amount_unowned", %{"items_per_page" => amount}, socket) do
+    limit = String.to_integer(amount)
+    params = %{socket.assigns.params | unowned_limit: limit}
+
+    socket =
+      socket
+      |> assign(:params, params)
+      |> push_patch(
+        to:
+          ~p"/adopcja/szukaja-domu?#{params}" <>
+            "#{Cat.Filter.to_params(socket.assigns.owned_filter, "owned_cat")}" <>
+            "#{Cat.Filter.to_params(socket.assigns.unowned_filter, "unowned_cat")}",
+        replace: true
+      )
+
+    {:noreply, socket}
+  end
+
+  @impl true
+  def handle_event("select_owned_page", %{"value" => page}, socket) do
+    params = %{socket.assigns.params | owned_page: page}
+
+    socket =
+      push_patch(socket,
+        to:
+          ~p"/adopcja/szukaja-domu?#{params}" <>
+            "#{Cat.Filter.to_params(socket.assigns.owned_filter, "owned_cat")}" <>
+            "#{Cat.Filter.to_params(socket.assigns.unowned_filter, "unowned_cat")}",
+        replace: true
+      )
+
+    {:noreply, socket}
+  end
+
+  @impl true
+  def handle_event("select_unowned_page", %{"value" => page}, socket) do
+    params = %{socket.assigns.params | unowned_page: page}
+
+    socket =
+      push_patch(
+        socket,
+        to:
+          ~p"/adopcja/szukaja-domu?#{params}" <>
+            "#{Cat.Filter.to_params(socket.assigns.owned_filter, "owned_cat")}" <>
+            "#{Cat.Filter.to_params(socket.assigns.unowned_filter, "unowned_cat")}",
+        replace: true
+      )
+
+    {:noreply, socket}
+  end
+
+  @impl true
   def handle_async(:load_unowned_cats, {:ok, cats}, socket) do
     socket =
       case cats do
-        {:ok, %Paged{items: unowned_cats, page_count: page_count, page_size: page_size, page: page, total: total}} ->
-          params = %{page: page, page_size: page_size}
+        {:ok, %Paged{items: unowned_cats, page_count: page_count, page_size: limit, page: page, total: total}} ->
+          params = Map.merge(socket.assigns.params, %{unowned_page: page, unowned_limit: limit})
           # NOTE: page_count = 0, page = 1 for no results when filteredcat_filt
           page_count = max(1, page_count)
 
@@ -36,9 +110,14 @@ defmodule KotkowoWeb.AdoptionLive.LookingForNewHome do
             |> stream(:unowned_cats, unowned_cats, reset: true)
             |> assign(:unowned_cats_total, total)
             |> assign(:unowned_page_count, page_count)
-            |> assign(:unowned_params, params)
+            |> assign(:params, params)
           else
-            push_navigate(socket, to: ~p"/adopcja/szukaja-domu")
+            push_patch(socket,
+              to:
+                ~p"/adopcja/szukaja-domu" <>
+                  "?#{Cat.Filter.to_params(socket.assigns.owned_filter, "owned_cat")}" <>
+                  "#{Cat.Filter.to_params(socket.assigns.unowned_filter, "unowned_cat")}"
+            )
           end
 
         {:error, msg} ->
@@ -53,8 +132,8 @@ defmodule KotkowoWeb.AdoptionLive.LookingForNewHome do
   def handle_async(:load_owned_cats, {:ok, cats}, socket) do
     socket =
       case cats do
-        {:ok, %Paged{items: owned_cats, page_count: page_count, page_size: page_size, page: page, total: total}} ->
-          params = %{page: page, page_size: page_size}
+        {:ok, %Paged{items: owned_cats, page_count: page_count, page_size: limit, page: page, total: total}} ->
+          params = Map.merge(socket.assigns.params, %{owned_page: page, owned_limit: limit})
           # NOTE: page_count = 0, page = 1 for no results when filtered
           page_count = max(1, page_count)
 
@@ -63,9 +142,14 @@ defmodule KotkowoWeb.AdoptionLive.LookingForNewHome do
             |> stream(:owned_cats, owned_cats, reset: true)
             |> assign(:owned_cats_total, total)
             |> assign(:owned_page_count, page_count)
-            |> assign(:owned_params, params)
+            |> assign(:params, params)
           else
-            push_navigate(socket, to: ~p"/adopcja/szukaja-domu")
+            push_patch(socket,
+              to:
+                ~p"/adopcja/szukaja-domu" <>
+                  "?#{Cat.Filter.to_params(socket.assigns.owned_filter, "owned_cat")}" <>
+                  "#{Cat.Filter.to_params(socket.assigns.unowned_filter, "unowned_cat")}"
+            )
           end
 
         {:error, msg} ->
@@ -83,14 +167,14 @@ defmodule KotkowoWeb.AdoptionLive.LookingForNewHome do
 
     socket =
       socket
-      |> assign(:initial_owned_filter, initial_owned_filter)
-      |> assign(:initial_unowned_filter, initial_unowned_filter)
+      |> assign(:owned_filter, initial_owned_filter)
+      |> assign(:unowned_filter, initial_unowned_filter)
       |> stream(:owned_cats, [])
       |> assign(:owned_cats_total, 0)
-      |> assign(:owned_params, nil)
+      |> assign(:params, %{})
       |> stream(:unowned_cats, [])
       |> assign(:unowned_cats_total, 0)
-      |> assign(:unowned_params, nil)
+      |> assign(:page, @first_page)
 
     {:ok, socket}
   end
@@ -100,11 +184,11 @@ defmodule KotkowoWeb.AdoptionLive.LookingForNewHome do
     owned_filter = params["owned_cat"] |> Cat.Filter.from_params() |> Map.put(:is_dead, false)
     unowned_filter = params["unowned_cat"] |> Cat.Filter.from_params() |> Map.put(:is_dead, false)
 
-    owned_page_size = params |> Map.get("owned_page_size", "30") |> parse_int_param()
+    owned_limit = params |> Map.get("owned_limit", "30") |> parse_int_param()
 
     owned_page = params |> Map.get("owned_page") |> parse_int_param()
 
-    unowned_page_size = params |> Map.get("unowned_page_size", "30") |> parse_int_param()
+    unowned_limit = params |> Map.get("unowned_limit", "30") |> parse_int_param()
 
     unowned_page = params |> Map.get("unowned_page") |> parse_int_param()
 
@@ -114,12 +198,12 @@ defmodule KotkowoWeb.AdoptionLive.LookingForNewHome do
       |> assign(:owned_filter, owned_filter)
       |> assign(:unowned_filter, unowned_filter)
       |> start_async(:load_owned_cats, fn ->
-        [page: owned_page, page_size: owned_page_size, filter: owned_filter]
+        [page: owned_page, page_size: owned_limit, filter: owned_filter]
         |> Client.new()
         |> Client.list_looking_for_adoption_cats(true)
       end)
       |> start_async(:load_unowned_cats, fn ->
-        [page: unowned_page, page_size: unowned_page_size, filter: unowned_filter]
+        [page: unowned_page, page_size: unowned_limit, filter: unowned_filter]
         |> Client.new()
         |> Client.list_looking_for_adoption_cats(false)
       end)
@@ -135,9 +219,8 @@ defmodule KotkowoWeb.AdoptionLive.LookingForNewHome do
     socket =
       push_patch(socket,
         to:
-          ~p"/adopcja/szukaja-domu" <>
-            "?#{owned_params}" <>
-            unowned_params,
+          ~p"/adopcja/szukaja-domu?#{socket.assigns.params}" <>
+            "#{owned_params}#{unowned_params}",
         replace: true
       )
 
@@ -152,9 +235,8 @@ defmodule KotkowoWeb.AdoptionLive.LookingForNewHome do
     socket =
       push_patch(socket,
         to:
-          ~p"/adopcja/szukaja-domu" <>
-            "?#{owned_filter}" <>
-            unowned_filter,
+          ~p"/adopcja/szukaja-domu?#{socket.assigns.params}" <>
+            "#{owned_filter}#{unowned_filter}",
         replace: true
       )
 
@@ -170,78 +252,5 @@ defmodule KotkowoWeb.AdoptionLive.LookingForNewHome do
       amount > 4 -> "#{amount} kotów"
       true -> "0 kotów"
     end
-  end
-
-  def pagination_bar(assigns) do
-    assigns =
-      assigns
-      |> assign(:previous_page, assigns.selected_page - 1)
-      |> assign(:next_page, assigns.selected_page + 1)
-      |> assign(:first_page, 1)
-
-    ~H"""
-    <div class="flex flex-row text-xl gap-x-4">
-      <button
-        :if={@selected_page != @first_page}
-        phx-click="select_page"
-        class="p-4"
-        value={@selected_page - 1}
-      >
-        <.icon class=" rotate-90 brightness-0" name="chevron_down" />
-      </button>
-
-      <.icon :if={@selected_page == @first_page} class="rotate-90 p-4 grayscale" name="chevron_down" />
-      <div class="self-center gap-4 hidden lg:inline">
-        <button :if={@selected_page > 2} class="w-14 h-14" phx-click="select_page" value={@first_page}>
-          <%= @first_page %>
-        </button>
-        <span :if={@selected_page > 2} class="w-14 h-14">...</span>
-        <button
-          :if={@selected_page != @first_page}
-          class="w-14 h-14"
-          phx-click="select_page"
-          value={@previous_page}
-        >
-          <%= @previous_page %>
-        </button>
-        <button class="font-bold w-14 h-14">
-          <%= @selected_page %>
-        </button>
-        <button
-          :if={@selected_page != @last_page}
-          class="w-14 h-14"
-          phx-click="select_page"
-          value={@next_page}
-        >
-          <%= @next_page %>
-        </button>
-        <span :if={@selected_page < @last_page - 1} class="w-14 h-14">...</span>
-
-        <button
-          :if={@selected_page < @last_page - 1}
-          class="w-14 h-14"
-          phx-click="select_page"
-          value={@last_page}
-        >
-          <%= @last_page %>
-        </button>
-      </div>
-      <div class="self-center flex flex-row lg:hidden gap-4">
-        <span><%= @selected_page %></span>
-        <span>z</span>
-        <span><%= @last_page %></span>
-      </div>
-      <.icon :if={@selected_page == @last_page} class="rotate-90 p-4 grayscale" name="chevron_up" />
-
-      <button
-        :if={@selected_page != @last_page}
-        class="p-4"
-        phx-click="select_page"
-        value={@selected_page + 1}
-      >
-        <.icon class="rotate-90 brightness-0" name="chevron_up" />
-      </button>
-    </div>
-    """
   end
 end
--- lib/kotkowo_web/live/news_live/passed_away.ex
+++ lib/kotkowo_web/live/news_live/passed_away.ex
@@ -3,6 +3,7 @@ defmodule KotkowoWeb.NewsLive.PassedAway do
 
   use KotkowoWeb, :live_view
 
+  import KotkowoWeb.Components.Pagination
   import KotkowoWeb.Components.Static.HowYouCanHelpSection
 
   alias Kotkowo.Client
@@ -23,12 +24,38 @@ defmodule KotkowoWeb.NewsLive.PassedAway do
   end
 
   @impl true
+  def handle_event("items_amount", %{"items_per_page" => amount}, socket) do
+    page_size = String.to_integer(amount)
+    params = %{socket.assigns.params | page_size: page_size}
+
+    socket =
+      push_patch(socket,
+        to: ~p"/aktualnosci/za-teczowym-mostem?#{params}" <> "#{Cat.Filter.to_params(socket.assigns.filter)}",
+        replace: true
+      )
+
+    {:noreply, socket}
+  end
+
+  @impl true
+  def handle_event("select_page", %{"value" => page}, socket) do
+    params = %{socket.assigns.params | page: page}
+
+    socket =
+      push_patch(socket,
+        to: ~p"/aktualnosci/za-teczowym-mostem?#{params}" <> "#{Cat.Filter.to_params(socket.assigns.filter)}",
+        replace: true
+      )
+
+    {:noreply, socket}
+  end
+
+  @impl true
   def handle_async(:load_cats, {:ok, cats}, socket) do
     socket =
       case cats do
         {:ok, %Paged{items: cats, page_count: page_count, page_size: page_size, page: page, total: total}} ->
           params = %{page: page, page_size: page_size}
-          # NOTE: page_count = 0, page = 1 for no results when filtered
           page_count = max(1, page_count)
 
           if page <= page_count do
@@ -38,7 +65,9 @@ defmodule KotkowoWeb.NewsLive.PassedAway do
             |> assign(:page_count, page_count)
             |> assign(:params, params)
           else
-            push_navigate(socket, to: ~p"/aktualnosci/za-teczowym-mostem")
+            push_patch(socket,
+              to: ~p"/aktualnosci/za-teczowym-mostem" <> "?#{Cat.Filter.to_params(socket.assigns.filter)}"
+            )
           end
 
         {:error, msg} ->
@@ -50,9 +79,8 @@ defmodule KotkowoWeb.NewsLive.PassedAway do
   end
 
   @impl true
-  def mount(params, _session, socket) do
-    initial_filter = Cat.Filter.from_params(params["cat"])
-    socket = socket |> assign(:initial_filter, initial_filter) |> stream(:cats, []) |> assign(:params, nil)
+  def mount(_params, _session, socket) do
+    socket = socket |> stream(:cats, []) |> assign(:params, nil)
     {:ok, socket}
   end
 
@@ -64,7 +92,9 @@ defmodule KotkowoWeb.NewsLive.PassedAway do
     page = params |> Map.get("page") |> parse_int_param()
 
     socket =
-      start_async(socket, :load_cats, fn ->
+      socket
+      |> assign(:filter, filter)
+      |> start_async(:load_cats, fn ->
         [page: page, page_size: page_size, filter: dead_filter] |> Client.new() |> Client.list_cats()
       end)
 
@@ -73,84 +103,12 @@ defmodule KotkowoWeb.NewsLive.PassedAway do
 
   @impl true
   def handle_info({:filter_cat, %Cat.Filter{} = filter}, socket) do
-    socket =
-      push_patch(socket, to: ~p"/aktualnosci/za-teczowym-mostem" <> "?#{Cat.Filter.to_params(filter)}", replace: true)
-
-    {:noreply, socket}
+    {:noreply,
+     push_patch(socket,
+       to: ~p"/aktualnosci/za-teczowym-mostem?#{socket.assigns.params}" <> "#{Cat.Filter.to_params(filter)}",
+       replace: true
+     )}
   end
 
   defp items_per_page, do: [30, 60, 90]
-
-  def pagination_bar(assigns) do
-    assigns =
-      assigns
-      |> assign(:previous_page, assigns.selected_page - 1)
-      |> assign(:next_page, assigns.selected_page + 1)
-      |> assign(:first_page, 1)
-
-    ~H"""
-    <div class="flex flex-row text-xl gap-x-4">
-      <button
-        :if={@selected_page != @first_page}
-        phx-click="select_page"
-        class="p-4"
-        value={@selected_page - 1}
-      >
-        <.icon class=" rotate-90 brightness-0" name="chevron_down" />
-      </button>
-
-      <.icon :if={@selected_page == @first_page} class="rotate-90 p-4 grayscale" name="chevron_down" />
-      <div class="self-center gap-4 hidden lg:inline">
-        <button :if={@selected_page > 2} class="w-14 h-14" phx-click="select_page" value={@first_page}>
-          <%= @first_page %>
-        </button>
-        <span :if={@selected_page > 2} class="w-14 h-14">...</span>
-        <button
-          :if={@selected_page != @first_page}
-          class="w-14 h-14"
-          phx-click="select_page"
-          value={@previous_page}
-        >
-          <%= @previous_page %>
-        </button>
-        <button class="font-bold w-14 h-14">
-          <%= @selected_page %>
-        </button>
-        <button
-          :if={@selected_page != @last_page}
-          class="w-14 h-14"
-          phx-click="select_page"
-          value={@next_page}
-        >
-          <%= @next_page %>
-        </button>
-        <span :if={@selected_page < @last_page - 1} class="w-14 h-14">...</span>
-
-        <button
-          :if={@selected_page < @last_page - 1}
-          class="w-14 h-14"
-          phx-click="select_page"
-          value={@last_page}
-        >
-          <%= @last_page %>
-        </button>
-      </div>
-      <div class="self-center flex flex-row lg:hidden gap-4">
-        <span><%= @selected_page %></span>
-        <span>z</span>
-        <span><%= @last_page %></span>
-      </div>
-      <.icon :if={@selected_page == @last_page} class="rotate-90 p-4 grayscale" name="chevron_up" />
-
-      <button
-        :if={@selected_page != @last_page}
-        class="p-4"
-        phx-click="select_page"
-        value={@selected_page + 1}
-      >
-        <.icon class="rotate-90 brightness-0" name="chevron_up" />
-      </button>
-    </div>
-    """
-  end
 end
