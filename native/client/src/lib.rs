82bf65506083fca959711503d463061aec2a752d
Pascal Jodlowski <jodlowskipascal@gmail.com>
2025-04-13 18:55:28 +0200

    :crab: views

--- native/client/src/lib.rs
+++ native/client/src/lib.rs
@@ -1,11 +1,13 @@
 use kotkowo_client::{
     get_advice_article as _get_advice_article, get_announcement_article, get_cat as get_kitty,
     get_cat_by_slug as get_kitty_by_slug, get_found_cat_by_slug as get_found_kitty_by_slug,
+    get_last_view_pull_utc as _get_last_view_pull_utc,
     get_lfh_cat_by_slug as get_lfh_kitty_by_slug, get_lost_cat_by_slug as get_lost_kitty_by_slug,
     list_adopted_cat, list_advice, list_announcement, list_cat, list_external_media as list_media,
-    list_found_cat, list_looking_for_adoption_cat, list_lost_cat, AdoptedCat, Advice, AdviceFilter,
-    Announcement, AnnouncementFilter, Article, BetweenDateTime, Cat, CatFilter, Error,
-    ExternalMedia, ExternalMediaFilter, FoundCat, LookingForHomeCat, LostCat, Options, Paged,
+    list_found_cat, list_looking_for_adoption_cat, list_lost_cat, update_views_and_last_pull,
+    AdoptedCat, Advice, AdviceFilter, Announcement, AnnouncementFilter, Article, BetweenDateTime,
+    Cat, CatFilter, Error, ExternalMedia, ExternalMediaFilter, FoundCat, LookingForHomeCat,
+    LostCat, Options, Paged, ViewUpdate,
 };
 
 #[rustler::nif]
@@ -84,10 +86,22 @@ fn get_cat(id: String) -> Result<Cat, Error> {
     get_kitty(id)
 }
 
+#[rustler::nif]
+fn get_last_view_pull_utc() -> Result<String, Error> {
+    _get_last_view_pull_utc()
+}
+
+#[rustler::nif]
+fn update_views(pull_date: String, updates: Vec<ViewUpdate>) -> Result<bool, Error> {
+    update_views_and_last_pull(pull_date, updates)
+}
+
 rustler::init!(
     "Elixir.Kotkowo.Client",
     [
         list_cats,
+        update_views,
+        get_last_view_pull_utc,
         list_lost_cats,
         list_found_cats,
         get_cat,
